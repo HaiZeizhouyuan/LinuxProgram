712b632d24b6768249874597558b6e8498bddbd7
zhouyuan <3623936813@qq.com>
2020-09-28 21:55:10 +0800

    ls

--- Operating-System/test/semget.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<sys/sem.h>
-#define MYKEY 6666
-int main() { 
-    int semid;
-    semid=semget(MYKEY,1,IPC_CREAT|0666);//创建了一个权限为666的信号量
-    printf("semid=%d\n",semid);
-    return 0;
-}
